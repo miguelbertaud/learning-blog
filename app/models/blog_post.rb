433c8b76c95187cee345e570be5620926dd2d330
miguel bertaud <miguel.bertaud@gmail>
2024-01-04 10:16:56 -0600

    Fix typo

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,6 +1,6 @@
 class BlogPost < ApplicationRecord
   validates :title, :body, presence: true
-  scope :sorted, -> { order(arel_table[:created_at].desc.nulls_last).order(update_at: :desc) }
+  scope :sorted, -> { order(arel_table[:created_at].desc.nulls_last).order(updated_at: :desc) }
   scope :draft, -> { where(published_at: nil) }
   scope :published, -> { where("published_at <= ?", Time.current) }
   scope :scheduled, -> { where("published_at > ?", Time.current) }
